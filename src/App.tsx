24989b41df4890b6068db510dc942c5f56b0f249
bguzman3-wq <bguzman3@uco.edu>
2026-09-09 10:05:46 -0500

    added an about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 
 export default function App() {
   return(
         <>
             <Hero/>
+            <About/>
         </>
     );
 }
