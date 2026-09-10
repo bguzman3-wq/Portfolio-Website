c4c806058e62a3cf9a704a13503927c0983d2270
bguzman3-wq <bguzman3@uco.edu>
2026-09-10 10:07:16 -0500

    Added contacts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contacts from "./components/Contacts";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
             <About/>
             <Projects/>
             <Skills/>
+            <Contacts/>
         </>
     );
 }
---
+++ src/components/Contacts.tsx
@@ -0,0 +1,17 @@
+export default function Contacts(){
+    return(
+        <section>
+            <h2>Contacts</h2>
+            <p>
+                <a href="https://www.linkedin.com/in/bryan-ceballos-guzman-513293296" target="_blank" rel="noopener noreferrer">
+                    LinkedIn
+                </a>
+            </p>
+            <p>
+                <a href="https://github.com/bguzman3-wq" target="_blank" rel="noopener noreferrer">
+                GitHub
+            </a>
+        </p>
+        </section>
+    );
+}
