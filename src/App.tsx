5cdf18672f8f77e8414843fbc26c41544cfb9a5c
bguzman3-wq <bguzman3@uco.edu>
2026-09-10 09:56:49 -0500

    Added skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
             <Hero/>
             <About/>
             <Projects/>
+            <Skills/>
         </>
     );
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,28 @@
+interface SkillGroup{
+    category: string;
+    items: string[];
+}
+
+const skillGroup: SkillGroup[] = [
+    {category: "Languages", items: ["Javascript", "TypeScript", "C++", "Python", "Java", "SQL", "R"]},
+    {category: "Frontend", items: ["React", "HTML", "CSS"]},
+    {category: "Tools", items: ["Git", "GitHub", "VS Code", "Vite"]},
+];
+
+export default function Skills(){
+    return (
+        <section> 
+            <h2>Skills</h2>
+            {skillGroup.map((group) => (
+                <div key={group.category}>
+                    <h3>{group.category}</h3>
+                    <ul>
+                        {group.items.map((item)=>(
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </section>
+    );
+}
