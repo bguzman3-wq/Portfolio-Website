302f28a28a3ba697eb58129bdd4aa814dc3ac81c
bguzman3-wq <bguzman3@uco.edu>
2026-09-09 22:26:56 -0500

    Added projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,11 @@
+import {Project} from '../types';
+
+const projects: Project[] = [
+    {
+        id: "1",
+        title: "Portfolio Website",
+        description: "A personal portfolio website built using React and TypeScript to showcase my projects and skills.",
+        link: "https://github.com/bguzman3-wq/portfolio-website",
+
+    },
+];
